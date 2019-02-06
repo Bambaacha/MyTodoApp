33e0106ae3ef63976e080c8ec8a1fe9f9cb1c7dd
Bambaacha <miltiadis_93@hotmail.de>
2019-02-06 12:19:01 +0100

    about component changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChucknorrisService } from '../chucknorris.service';
 
 @Component({
   selector: 'app-about',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  jokeText = '';
 
-  constructor() { }
+  constructor(chucknorrisService: ChucknorrisService) { 
+    this.jokeText = chucknorrisService.lastJokeText
+  }
 
   ngOnInit() {
   }
